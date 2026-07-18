56bf3ce900e8df2caca9c5fea94e550455b99640
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-07-18 11:42:53 +0600

    added api keys

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express = require("express");
+import dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
